f20c495182380ee1725acbbc6b43a0bdcebe8eb0
Paulo Ohno Git <pauloohnogit@gmail.com>
2013-03-29 21:43:05 -0300

    Source files

---
+++ src/main/DateTest.java
@@ -0,0 +1,17 @@
+package main;
+
+import java.util.Date;
+
+public class DateTest {
+
+    public static void main(String[] args) {
+
+	Date data = new Date();
+
+	System.out.println("Data: " + data.toString());
+
+	System.out.println("Changed for Git Testing");
+
+    }
+
+}
